c0f9b2363279d923a74698f69a4269f504087dfb
Amrit Raj <amrit.raj@scribefi.com>
2022-10-27 17:33:00 +0530

    Chnages

--- index.js
+++ index.js
@@ -12,24 +12,24 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/userRoutes"));
 app.use("/api/messages", require("./routes/messagesRoute"));
 
+const PORT = process.env.PORT || 8000;
 
 
-const __dirname1 = path.resolve();
+if (process.env.NODE_ENV == "production") {
+  const __dirname1 = path.resolve();
+  app.use(express.static(path.join(__dirname1, "/client/build")));
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname1, "/client/build")));
-
-    app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
-    );
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+  );
 } else {
-    app.get("/", (req, res) => {
-        res.send("API is running..");
-    });
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
 }
 
-const server = app.listen(process.env.port, () => {
-  console.log(`server is running on port ${process.env.port}`);
+const server = app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
 });
 
 const io = socket(server, {
@@ -39,7 +39,7 @@ const io = socket(server, {
   },
 });
 
-app.use(express.static(path.join(__dirname, "./build")));
+// app.use(express.static(path.join(__dirname, "./build")));
 
 global.onlineUsers = new Map();
 io.on("connection", (socket) => {
